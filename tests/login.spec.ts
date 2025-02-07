cc6c70009fc2738a19c5889d4e9c3f407555e75e
prle <przemyslaw.leszczynski@softwaremind.com>
2025-02-07 21:03:02 +0100

    added prettier

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,31 +1,32 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('User login to Demobank', () => {
+  test('successful login with correct credentials', async ({ page }) => {
+    //Arrange
+    const url = 'https://demo-bank.vercel.app/';
+    const userID = 'testerLO';
+    const userPassword = '12345678';
+    const expectedUserName = 'Jan Demobankowy';
 
-test('successful login with correct credentials', async ({ page }) => {
-  //Arrange
-  const url = 'https://demo-bank.vercel.app/';
-  const userID = 'testerLO';
-  const userPassword = '12345678';
-  const expectedUserName = 'Jan Demobankowy';
+    //Act
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userID);
+    await page.getByTestId('password-input').fill(userPassword);
+    await page.getByTestId('login-button').click();
 
-  //Act
-  await page.goto(url);
-  await page.getByTestId('login-input').fill(userID);
-  await page.getByTestId('password-input').fill(userPassword);
-  await page.getByTestId('login-button').click();
+    //Assert
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
+  });
 
-  //Assert
-  await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
-});
+  test('unsuccessful login with too shart username', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').fill('test');
+    await page.getByTestId('password-input').click();
 
-test('unsuccessful login with too shart username', async ({ page }) => {
-  await page.goto('https://demo-bank.vercel.app/');
-  await page.getByTestId('login-input').fill('test');
-  await page.getByTestId('password-input').click();
-
-  await expect(page.getByTestId('error-login-id')).toHaveText('identyfikator ma min. 8 znaków');
-});
+    await expect(page.getByTestId('error-login-id')).toHaveText(
+      'identyfikator ma min. 8 znaków',
+    );
+  });
 
   test('unsuccessful login with too short password', async ({ page }) => {
     await page.goto('https://demo-bank.vercel.app/');
@@ -35,7 +36,7 @@ test('unsuccessful login with too shart username', async ({ page }) => {
     await page.getByTestId('password-input').blur();
 
     await expect(page.getByTestId('error-login-password')).toHaveText(
-      'hasło ma min. 8 znaków'
+      'hasło ma min. 8 znaków',
     );
   });
-}); 
+});
--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -3,15 +3,14 @@ import { test, expect } from '@playwright/test';
 test.describe('Pulpit tests', () => {
   test('quick payment with correct data', async ({ page }) => {
     //Arrange
-    const url = "https://demo-bank.vercel.app/";
-    const userID = "testerLO";
-    const userPassword = "12345678";
+    const url = 'https://demo-bank.vercel.app/';
+    const userID = 'testerLO';
+    const userPassword = '12345678';
 
-    const receiverID = "2";
-    const transferAmount = "666";
+    const receiverID = '2';
+    const transferAmount = '666';
     const transferTitle = 'pizza';
     const expectedTransferReceiver = 'Chuck Demobankowy';
-
 
     //Act
     await page.goto(url);
@@ -21,17 +20,17 @@ test.describe('Pulpit tests', () => {
 
     await page.waitForLoadState('domcontentloaded');
 
-      const receiverId = '2';
+    const receiverId = '2';
     await page.locator('#widget_1_transfer_receiver').selectOption(receiverId);
     await page.locator('#widget_1_transfer_amount').fill(transferAmount);
     await page.locator('#widget_1_transfer_title').fill(transferTitle);
 
     await page.getByRole('button', { name: 'wykonaj' }).click();
     await page.getByTestId('close-button').click();
-    
+
     //Assert
     await expect(page.locator('#show_messages')).toHaveText(
-      `Przelew wykonany! ${expectedTransferReceiver} - ${666},00PLN - ${transferTitle}`
+      `Przelew wykonany! ${expectedTransferReceiver} - ${666},00PLN - ${transferTitle}`,
     );
   });
 
@@ -40,14 +39,15 @@ test.describe('Pulpit tests', () => {
     await page.getByTestId('login-input').fill('testerLO');
     await page.getByTestId('password-input').fill('12345678');
     await page.getByTestId('login-button').click();
-    
+
     await page.locator('#widget_1_topup_receiver').selectOption('500 xxx xxx');
     await page.locator('#widget_1_topup_amount').fill('666');
     await page.locator('#uniform-widget_1_topup_agreement span').click();
     await page.getByRole('button', { name: 'doładuj telefon' }).click();
     await page.getByTestId('close-button').click();
 
-
-    await expect (page.locator('#show_messages')).toHaveText('Doładowanie wykonane! 666,00PLN na numer 500 xxx xxx');
+    await expect(page.locator('#show_messages')).toHaveText(
+      'Doładowanie wykonane! 666,00PLN na numer 500 xxx xxx',
+    );
   });
 });
